9c4fb31bb922dc650e971c57b4d8376cbd43af06
Ivan <ivanveselinov22@gmail.com>
2022-01-06 11:10:54 +1100

    like done

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost } from '../contollers/posts.js'
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../contollers/posts.js'
 
 const router = express.Router(); // createing router
       
@@ -9,6 +9,7 @@ router.post('/', createPost);     // Write Post
 // router.get('/:id', getPosts);  // get post by id
 router.patch('/:id', updatePost);  // Update Post
 router.delete('/:id', deletePost); // Delete Post
+router.patch('/:id/likePost', likePost); //Like Post
 export default router;
 
 
